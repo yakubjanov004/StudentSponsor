89703ef9fd23049a6b6003e31f9a8e4b1b8c9b0e
yakubjanov004 <ulugbekyoqubjonov59@gmail.com>
2024-05-11 21:01:15 +0500

    static root

--- config/settings.py
+++ config/settings.py
@@ -72,12 +72,6 @@ TEMPLATES = [
     },
 ]
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
@@ -128,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
