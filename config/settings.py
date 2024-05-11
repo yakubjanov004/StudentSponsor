718bc5ce32ee6e7b3ce83644b42fb73b44371840
yakubjanov004 <ulugbekyoqubjonov59@gmail.com>
2024-05-11 20:58:06 +0500

    static root

--- config/settings.py
+++ config/settings.py
@@ -71,6 +71,13 @@ TEMPLATES = [
         },
     },
 ]
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
